df9453fb5b9d7c94a0925af4768eaf10aa710d92
Daniel <abonaparte3@yahoo.com>
2015-04-29 22:35:58 +0000

    Updated page layouts and Resized Images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,12 +8,16 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   version :medium do
     process :eager => true
-    process :resize_to_fill => [250, 250, :face]
+    process :resize_to_fill => [400, 400, :face]
   end
   
   version :wide do
     process :eager => true
     process :resize_to_fill => [451, 250]
   end
+  
+  version :thumb do
+    process :resize_to_fill => [200, 200]
+end
   
 end
