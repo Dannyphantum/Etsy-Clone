68a98c7968190b81699f8e8d1344b3828842d061
Daniel <abonaparte3@yahoo.com>
2015-04-30 22:53:50 +0000

    Added Validations

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -7,7 +7,7 @@ module DeviseHelper
     sentence = I18n.t('errors.messages.not_saved', count: resources.errors.count, resource: resource.class.model_name.human.downcase)
     
     html = <<-HTML
-<div class="alert alert-danger alert-dissmissable">
+<div class="alert alert-danger alert-dismissable">
 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 
 <h4>#{sentence}</h4>
